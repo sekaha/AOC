36c7b0f3c7a982e42ffbf192855952fcdc0c6eb2
Hakase <davidsommerfield.zg@gmail.com>
2023-12-15 22:54:58 -0600

    pure math solution change

--- Python/day6p2.py
+++ Python/day6p2.py
@@ -1,34 +1,7 @@
+from math import ceil, floor
+
 with open("res/day6input.txt") as file:
     time, distance = [int("".join(l.split()[1:])) for l in file]
-
-    lowest, highest = 0, 0
-    l, r = 0, time // 2
-
-    # find lowest hold time that will win
-    while l <= r:
-        m = l + (r - l) // 2
-        traveled = (time - m) * m
-
-        if traveled > distance:
-            lowest = m
-            r = m - 1
-        else:
-            l = m + 1
-
-    # find highest hold time that will win
-    l, r = time // 2, time
-
-    # find lowest hold time that will win
-    while l <= r:
-        m = l + (r - l) // 2
-        traveled = (time - m) * m
-
-        if traveled > distance:
-            highest = m
-            l = m + 1
-        else:
-            r = m - 1
-
-    possibilites = highest - lowest + 1
-
-print(possibilites)
+    lowest = ceil((-time + (time**2 - 4 * distance) ** 0.5) / -2)
+    highest = floor((-time - (time**2 - 4 * distance) ** 0.5) / -2)
+    print(highest - lowest + 1)
